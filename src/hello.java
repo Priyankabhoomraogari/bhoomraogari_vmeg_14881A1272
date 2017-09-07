13f6fd79bb85118e600c43500ff49d92a2e7fe68
Priyankabhoomraogari <31738755+Priyankabhoomraogari@users.noreply.github.com>
2017-09-07 22:04:54 +0530

    Adding code
    //

---
+++ src/hello.java
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
